8c3a98e7b183923255a80114effc1c4245a2c41d
Pranav Rajan <rajan.pranav320@gmail.com>
2023-12-06 20:48:40 -0800

    Add typescript

---
+++ typescript/dist/index.js
@@ -0,0 +1,17 @@
+"use strict";
+let age = 30;
+let firstName = 'Mario';
+let isFictional;
+age = 31;
+firstName = 'Luigi';
+isFictional = false;
+let planet = 'Earth';
+let moons = 1;
+let isLarge = false;
+planet = 'Saturn';
+moons = 145;
+// null & unbdefined 
+let something;
+let anotherThing;
+console.log('HellO, World!');
+console.log('this is content');
---
+++ typescript/src/index.ts
@@ -0,0 +1,20 @@
+let age: number = 30;
+let firstName: string = 'Mario';
+let isFictional: boolean;
+
+age = 31;
+firstName = 'Luigi';
+isFictional = false;
+
+let planet = 'Earth';
+let moons = 1; 
+let isLarge = false;
+planet = 'Saturn';
+moons = 145
+
+// null & unbdefined 
+let something: null 
+let anotherThing: undefined;
+
+console.log('HellO, World!');
+console.log('this is content');
