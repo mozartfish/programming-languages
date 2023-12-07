57193fa64599a7958e9974c3586ec4b82e35e5dc
Pranav Rajan <rajan.pranav320@gmail.com>
2023-12-06 21:53:13 -0800

    experiment with golang modules

---
+++ golang/lucky-number/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// Get a random numbet between 0 and 99 inclusive
+	// n := rand.Intn(100)
+	fmt.Printf("Your lucky number is %d!\n", randomNumber())
+}
