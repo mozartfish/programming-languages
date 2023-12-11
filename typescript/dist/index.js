05c0966ba3d95d8b6ebb5c814bc286eeb0c304db
Pranav Rajan <rajan.pranav320@gmail.com>
2023-12-10 19:45:38 -0800

    experiment with some more typescript stuff

--- typescript/dist/index.js
+++ typescript/dist/index.js
@@ -1,4 +1,12 @@
 "use strict";
+const authorOne = { name: 'mario', avatar: '/img/mario.png' };
+const newPost = {
+    title: 'my first post',
+    body: 'this is content',
+    tags: ['gaming', 'text'],
+    create_at: new Date(),
+    author: authorOne,
+};
 let age = 30;
 let firstName = 'Mario';
 let isFictional;
@@ -10,8 +18,29 @@ let moons = 1;
 let isLarge = false;
 planet = 'Saturn';
 moons = 145;
-// null & unbdefined 
+// null & unbdefined
 let something;
 let anotherThing;
-console.log('HellO, World!');
-console.log('this is content');
+// arrays
+let names = [
+    'harry potter',
+    'lord voldemort',
+    'dumbledore',
+    'snape',
+    'hagrid',
+    'hermione',
+    'ron',
+];
+let ages = [25, 24, 10];
+names.push('this is a test');
+ages.push(1);
+//object literals
+const g = names[2];
+console.log(g);
+//type inference with object literals
+let user = {
+    firstName: 'hello',
+    age: 30,
+    id: 10,
+};
+console.log(user);
--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,3 +1,25 @@
+// interfaces
+interface Author {
+  name: string;
+  avatar: string;
+}
+const authorOne: Author = { name: 'mario', avatar: '/img/mario.png' };
+interface Post {
+  title: string;
+  body: string;
+  tags: string[];
+  create_at: Date;
+  author: Author;
+}
+
+const newPost: Post = {
+  title: 'my first post',
+  body: 'this is content',
+  tags: ['gaming', 'text'],
+  create_at: new Date(),
+  author: authorOne,
+};
+
 let age: number = 30;
 let firstName: string = 'Mario';
 let isFictional: boolean;
@@ -7,14 +29,36 @@ firstName = 'Luigi';
 isFictional = false;
 
 let planet = 'Earth';
-let moons = 1; 
+let moons = 1;
 let isLarge = false;
 planet = 'Saturn';
-moons = 145
+moons = 145;
 
-// null & unbdefined 
-let something: null 
+// null & unbdefined
+let something: null;
 let anotherThing: undefined;
 
-console.log('HellO, World!');
-console.log('this is content');
+// arrays
+let names: string[] = [
+  'harry potter',
+  'lord voldemort',
+  'dumbledore',
+  'snape',
+  'hagrid',
+  'hermione',
+  'ron',
+];
+let ages: number[] = [25, 24, 10];
+names.push('this is a test');
+ages.push(1);
+//object literals
+const g = names[2];
+console.log(g);
+//type inference with object literals
+let user: { firstName: string; age: number; id: number } = {
+  firstName: 'hello',
+  age: 30,
+  id: 10,
+};
+
+console.log(user);
