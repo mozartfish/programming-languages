1b09917b5432500822474d186388ac2aab8cf7ad
Pranav Rajan <rajan.pranav320@gmail.com>
2023-12-21 09:13:58 -0800

    Day 1 Golang

---
+++ golang/main.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+	helloWorld()
+	values()
+	variables()
+	constants()
+	forConstruct()
+	ifElseConstruct()
+	switchConstruct()
+	arrayConstruct()
+	slicesConstruct()
+	mapConstruct()
+	rangeConstruct()
+}
